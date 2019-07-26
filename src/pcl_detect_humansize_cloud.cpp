f02907f0166f99fd012df80a1e69f5dcbba80109
taishiyamamoto <ambitious0816@gmail.com>
2019-07-27 01:13:26 +0900

    delete no use header

--- src/pcl_detect_humansize_cloud.cpp
+++ src/pcl_detect_humansize_cloud.cpp
@@ -12,8 +12,6 @@
 #include <pcl/segmentation/conditional_euclidean_clustering.h>
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/segmentation/impl/conditional_euclidean_clustering.hpp>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
