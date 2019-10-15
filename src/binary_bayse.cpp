d9a15a5253dce7d394409e813ed8977bfe3120c1
taishiyamamoto <ambitious0816@gmail.com>
2019-10-16 02:17:27 +0900

    chagnge binary_bayse

--- src/binary_bayse.cpp
+++ src/binary_bayse.cpp
@@ -2,111 +2,102 @@
 #include <tf/transform_listener.h>
 #include<geometry_msgs/PointStamped.h>
 #include<geometry_msgs/Point.h>
-#include<jsk_recognition_msgs/SimpleOccupancyGridArray.h>
+#include<nav_msgs/OccupancyGrid.h>
 #include<cmath>
 
 class BinaryBayse{
     public:
         BinaryBayse(){
-            sub_=nh_.subscribe<geometry_msgs::PointStamped>("target_point",1,&BinaryBayse::callback,this);
-            pub2_=nh_.advertise<jsk_recognition_msgs::SimpleOccupancyGridArray>("simple_grid",1);
+            map_flag_=false;
+            ros::NodeHandle pnh_("~");
+            pnh_.param<float>("resolution",map_.info.resolution,0.20);
+            pnh_.param<double>("target_prob",target_prob_,0.55);
+            pnh_.param<double>("target_thresh",target_thresh_,0.90);
+            pub_=nh_.advertise<geometry_msgs::PointStamped>("filtered_target_point",1);
+            sub_=nh_.subscribe<geometry_msgs::PointStamped>("target_point",1,&BinaryBayse::pointcallback,this);
+            sub2_=nh_.subscribe<nav_msgs::OccupancyGrid>("map_for_searcharea",100,&BinaryBayse::mapcallback,this);
             tf_listener_ = new tf::TransformListener();
-            matrix.resize(nx_,std::vector<double>(nx_));
         }
 
     private:
-        void callback(const geometry_msgs::PointStampedConstPtr &target_point);
+        void pointcallback(const geometry_msgs::PointStampedConstPtr &target_point);
+        void mapcallback(const nav_msgs::OccupancyGridConstPtr &map);
+        int coord_to_cell(const double x,double y);
+        double prob_to_exp(const double prob);
+        double exp_to_prob(const double exp);
         ros::NodeHandle nh_;
         ros::Subscriber sub_;
+        ros::Subscriber sub2_;
         ros::Publisher pub_;
-        ros::Publisher pub2_;
 
         tf::TransformListener *tf_listener_;
 
-        jsk_recognition_msgs::SimpleOccupancyGridArray grid_array;
-        jsk_recognition_msgs::SimpleOccupancyGrid grid;
+        std::string robot_frame_;
+        std::string map_frame_;
+
         geometry_msgs::Point point;
-
-        double cell_size_=0.2;
-        double search_range_ = 3.5;
-        int nx_= std::round(search_range_/ cell_size_)*2;
-        std::vector<std::vector<double>> matrix;
-
-        double target_prob_ = 0.55;
-        double decay_prob_ = 0.45;
+        nav_msgs::OccupancyGrid map_;
+        std::vector<double> target_cell_;
+        double target_thresh_;
+        double target_prob_;
+        bool map_flag_;
 };
 
+int
+BinaryBayse::coord_to_cell(const double x,double y){
+    int cell_x=(map_.info.width/2)+std::round(x/map_.info.resolution), 
+        cell_y=(map_.info.height/2)+std::round(y/map_.info.resolution);
+    return map_.info.width*(cell_y-1)+cell_x+1;
+}
+
+void
+BinaryBayse::mapcallback(const nav_msgs::OccupancyGridConstPtr &map){
+    double resolution_rate=map->info.resolution/map_.info.resolution;
+    map_.header=map->header;
+    map_.info.origin.position.x=map->info.origin.position.x*resolution_rate;
+    map_.info.origin.position.y=map->info.origin.position.y*resolution_rate;
+    map_.info.origin.position.z=map->info.origin.position.z*resolution_rate;
+    map_.info.height=map->info.height*resolution_rate;
+    map_.info.width=map->info.width*resolution_rate;
+    target_cell_.resize(map_.info.height*map_.info.width);
+    std::cout<<resolution_rate<<std::endl;
+    std::cout<<map_.info.origin.position.x<<std::endl;
+    std::cout<<map_.info.height<<std::endl;
+    std::cout<<map_.info.width<<std::endl;
+    std::cout<<target_cell_.size()<<std::endl;
+    map_flag_=true;
+}
+
+double
+BinaryBayse::prob_to_exp(const double prob){
+    return std::log(prob/(1-prob));
+}
+
+double
+BinaryBayse::exp_to_prob(const double exp){
+    return 1/(1+std::exp(-exp));
+}
+
 void 
-BinaryBayse::callback(const geometry_msgs::PointStampedConstPtr &target_point){
-    geometry_msgs::PointStamped trans_point;
-    geometry_msgs::PointStamped conv_point;
+BinaryBayse::pointcallback(const geometry_msgs::PointStampedConstPtr &target_point){
+    geometry_msgs::PointStamped filtered_target_point;
+    if(!map_flag_){
+        ROS_ERROR("no map received");
+        return;
+    }
 
-    trans_point.header.frame_id=target_point->header.frame_id;
-    trans_point.point.x=target_point->point.x;
-    trans_point.point.y=target_point->point.y;
+    int cell=coord_to_cell(target_point->point.x,target_point->point.y);
 
-	try{
-		tf_listener_->waitForTransform("base_link", target_point->header.frame_id, ros::Time(0), ros::Duration(100.0));
-	}catch(tf::TransformException ex){
-		ROS_ERROR("%s",ex.what());
-	}
-    /*
-    std::cout<<"target_point"<<std::endl;
-    std::cout<<target_point->point.x<<std::endl;
-    std::cout<<target_point->point.y<<std::endl;
-    */
-    try{
-	    tf_listener_->transformPoint("base_link",trans_point, conv_point);
-		}
-		catch(tf::TransformException &e){
-			ROS_WARN_STREAM("tf::TransformException: " << e.what());
-		}
-    
-    std::cout<<"conv_point"<<std::endl;
-    std::cout<<conv_point.point.x<<std::endl;
-    std::cout<<conv_point.point.y<<std::endl;
-    
-    int x_cell = std::round(conv_point.point.x/cell_size_+(search_range_/cell_size_));
-    int y_cell = std::round(conv_point.point.y/cell_size_+(search_range_/cell_size_));
+    target_cell_[cell]+=prob_to_exp(target_prob_);
 
-    matrix[x_cell][y_cell]+=std::log(target_prob_/(1-target_prob_));
-    //matrix[x_cell][y_cell]+=1;
-    //std::cout<< matrix[x_cell][y_cell]<<std::endl;
+    ROS_INFO("cell:%d, prob:%lf",cell,exp_to_prob(target_cell_[cell]));
 
-    for(int i=0;i<nx_;i++){
-        for(int j=0;j<nx_;j++){
-            //matrix[i][j] += std::log(decay_prob_/(1-decay_prob_));
-
-            if(1/(1+exp(-matrix[i][j]))>0.9){
-
-                //std::cout<<matrix[i][j]<<std::endl;
-                std::cout<<"x:"<<i<<" y:"<<j<<" prob:"<<1/(1+exp(-matrix[i][j]))<<std::endl;
-                //std::cout<<"x:"<<i<<" y:"<<j<<" value:"<<matrix[i][j]<<std::endl;
-
-                double x = (i-(search_range_/cell_size_))*cell_size_;
-                double y = (j-(search_range_/cell_size_))*cell_size_;
-
-                point.x=x;
-                point.y=y;
-                point.z=0;
-                grid_array.header.frame_id="base_link";
-                grid.header.frame_id="base_link";
-                grid.resolution=0.2;
-                grid.coefficients[0]=0;
-                grid.coefficients[1]=0;
-                grid.coefficients[2]=0;
-                grid.coefficients[3]=0;
-                grid.cells.push_back(point);
-                grid_array.grids.push_back(grid);
-                pub2_.publish(grid_array);
-
-
-                std::cout<<"x y"<<std::endl;
-                std::cout<<x<<std::endl;
-                std::cout<<y<<std::endl;
-
-            }
-        }
+    if(exp_to_prob(target_cell_[cell])>=target_thresh_){
+        ROS_INFO("find_target");
+        filtered_target_point=*target_point;
+        std::cout<<filtered_target_point.point.x<<std::endl;
+        pub_.publish(filtered_target_point);
+        target_cell_[cell]=0;
     }
 
 }
